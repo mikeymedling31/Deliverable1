b23a6fdb01d4b69e3e08c5b4ee9c20c5079e92e5
Michael Medling <mikeymedling31@gmail.com>
2020-09-09 13:28:22 -0400

    Update ValidatePassword.java
    Revision of loop.

--- src/ValidatePassword.java
+++ src/ValidatePassword.java
@@ -8,38 +8,44 @@ public class ValidatePassword {
 		Scanner input = new Scanner (System.in);
 		
 		String createdPassword;
-		
-		System.out.println("Please create a createdPassword.\n\n" 
-				+ "The createdPassword must contain:\n"
-				+ " * At least one lowercase letter\n"
-				+ " * At least one uppercase letter\n"
-				+ " * At least minimum 7 characters\n" 
-				+ " * At least maximum 12 characters\n" 
-				+ " * An exclamation point: !\n\n"
-				+ "Enter new createdPassword:"); 
-		
-		createdPassword = input.next();
-		if(Pattern.matches(".*[!].*", createdPassword)){
-			if(Pattern.matches(".*[A-Z].*", createdPassword)) {
-				if(Pattern.matches(".*[a-z].*", createdPassword)) {
-					if(createdPassword.length() >= 7 && createdPassword.length() <= 12) {
-					System.out.println("Password valid and accepted!");
-					}else{
-						System.out.println("ERROR Password must be less than 12 and more than 7 characters in length");
-						main(args);
+		boolean flag = false;
+		while(!flag) {
+			System.out.println("Please create a createdPassword.\n\n" 
+					+ "The createdPassword must contain:\n"
+					+ " * At least one lowercase letter\n"
+					+ " * At least one uppercase letter\n"
+					+ " * At least minimum 7 characters\n" 
+					+ " * At least maximum 12 characters\n" 
+					+ " * An exclamation point: !\n\n"
+					+ "Enter new createdPassword:"); 
+			
+			createdPassword = input.next();
+			if(Pattern.matches(".*[!].*", createdPassword)){
+				
+				if(Pattern.matches(".*[A-Z].*", createdPassword)) {
+					
+					if(Pattern.matches(".*[a-z].*", createdPassword)) {
+						
+						if(createdPassword.length() >= 7 && createdPassword.length() <= 12) {
+							System.out.println("Password valid and accepted!");
+							flag = true;
+						} else {
+							System.out.println("ERROR Password must be more than 7 characters and less than 12 characters.");
+							flag = false;
+						}
+					} else {
+						System.out.println("ERROR Password must have at least one lowercase character.");
+						flag = false;
 					}
-				}else {
-					System.out.println("ERROR Password must have atleast one lowercase character");
-					main(args);
+				} else {
+					System.out.println("ERROR Password must have at least one uppercase character.");
+					flag = false;
 				}
-			}else {
-				System.out.println("ERROR Password must have atleast one uppercase character");
-				main(args);
+			} else {
+				System.out.println("ERROR Password must have at least one \"!\".");
+				flag = false;
 			}
-		}else {
-			System.out.println("ERROR Password must have atleast one !");
-			main(args);
-		}
-		
+		}	
+		input.close();		
 	}
 }
